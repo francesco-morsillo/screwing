9f75e9977fe4f312b8c06c04edc60b6bde418640
Francesco Morsillo <fmorsill@laas.fr>
2013-07-05 10:09:28 +0200

    MoveRightHand perfectly working on OpenHRP

--- openHRP/example.py
+++ openHRP/example.py
@@ -18,7 +18,7 @@ The needed part is strictly the task definition and the push in the stack of tas
 from dynamic_graph.sot.core.matrix_util import matrixToTuple
 from dynamic_graph.sot.core.meta_tasks_kine import MetaTaskKine6d
 from dynamic_graph.sot.core.meta_tasks import gotoNd
-from numpy import *
+from numpy import eye
 
 def moveRightHandToTarget(robot,solver,target):
 	
@@ -26,6 +26,7 @@ def moveRightHandToTarget(robot,solver,target):
 	taskRH=MetaTaskKine6d('rh',robot.dynamic,'rh','right-wrist')
 	handMgrip=eye(4); handMgrip[0:3,3] = (0,0,-0.21)
 	taskRH.opmodif = matrixToTuple(handMgrip)
+	taskRH.feature.frame('desired')
 
 	# Reference and gain setting
 	gotoNd(taskRH,target,'000111',(5,1,0.01,0.9))
