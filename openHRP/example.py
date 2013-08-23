506078a3b50c6f80ff533be43e6d0b6369d09945
Francesco Morsillo <fmorsill@laas.fr>
2013-08-23 16:36:38 +0200

    Add posture in openHRP/example

--- openHRP/example.py
+++ openHRP/example.py
@@ -17,20 +17,27 @@ The needed part is strictly the task definition and the push in the stack of tas
 
 from dynamic_graph.sot.core.matrix_util import matrixToTuple
 from dynamic_graph.sot.core.meta_tasks_kine import MetaTaskKine6d
+from dynamic_graph.sot.core.meta_task_posture import MetaTaskKinePosture
 from dynamic_graph.sot.core.meta_tasks import gotoNd
 from numpy import eye
 
 def moveRightHandToTarget(robot,solver,target):
 	
 	# TASKRH Definition
-	taskRH=MetaTaskKine6d('rh',robot.dynamic,'rh','right-wrist')
+	mTaskRH=MetaTaskKine6d('rh',robot.dynamic,'rh','right-wrist')
 	handMgrip=eye(4); handMgrip[0:3,3] = (0,0,-0.21)
-	taskRH.opmodif = matrixToTuple(handMgrip)
-	taskRH.feature.frame('desired')
+	mTaskRH.opmodif = matrixToTuple(handMgrip)
+	mTaskRH.feature.frame('desired')
 
 	# Reference and gain setting
-	gotoNd(taskRH,target,'000111',(5,1,0.01,0.9))
+	gotoNd(mTaskRH,target,'000111',(5,1,0.01,0.9))
+
+
+	# Task Posture
+	mTaskPosture = MetaTaskKinePosture(robot.dynamic)
+	mTaskPosture.ref = robot.halfSitting
 
 	# push
-	solver.sot.push(taskRH.task.name)
+	solver.sot.push(mTaskRH.task.name)
+	solver.sot.push(mTaskPosture.task.name)
 
